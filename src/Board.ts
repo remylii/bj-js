0c6052432b7b1175b9f70b1e0a44965f722c4b75
remyli <rapi.remyli@gmail.com>
2019-09-27 11:54:51 +0900

    update

--- src/Board.ts
+++ src/Board.ts
@@ -13,4 +13,25 @@ export class Board {
         const total_cell_count = n**2;
         this.data = new Array<string|null>(total_cell_count).fill(null);
     }
+
+    getViewData(): Array<string|null> {
+        return this.data;
+    }
+
+    put(x: number, y: number, block: string): void {
+        if (0 > x || x >= this.length) {
+            throw Error(`Invalid argument x`);
+        }
+
+        if (0 > y || y >= this.length) {
+            throw Error(`Invalid argument y`);
+        }
+
+        const idx = x + (y * this.length);
+        if (this.data[x] !== null) {
+            throw Error(`Already declared`);
+        }
+
+        this.data[idx] = block;
+    }
 }
--- tests/board.test.ts
+++ tests/board.test.ts
@@ -4,4 +4,18 @@ describe('Board', (): void => {
     test('constructor', (): void => {
         const b = new Board(10);
     });
+
+    test(`put`, (): void => {
+        const board = new Board(10);
+        const s1 = '●';
+        const s2 = '◆';
+
+        board.put(9, 9, s1);
+        board.put(0, 0, s2);
+
+        const s1_idx = board.getViewData().indexOf(s1);
+        const s2_idx = board.getViewData().indexOf(s2);
+        expect(s1_idx).toBe(99);
+        expect(s2_idx).toBe(0);
+    });
 });
