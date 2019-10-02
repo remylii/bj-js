b9449b4bbe76341cd7f1080a1a91b2fbaf0c2671
remyli <rapi.remyli@gmail.com>
2019-10-02 11:39:13 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-function hello(): string {
-    return `Hello`;
-}
+import { Board } from '#/Board';
 
-const elem = document.querySelector(".app");
-if (!!elem) {
-    elem.innerHTML = hello();
-}
+const board = new Board(10);
+const elements = document.querySelectorAll('.item');
+
+elements.forEach((elem: Element) => {
+    elem.addEventListener('click', () => {
+        const alls: Array<Element> = [].slice.call(elements);
+        console.log(alls);
+        elem.innerHTML = `` + alls.indexOf(elem);
+    });
+});
