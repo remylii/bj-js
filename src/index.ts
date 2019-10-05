236a804e3ade087b5750b0332bdcedf0563c563b
remyli <rapi.remyli@gmail.com>
2019-10-05 23:17:39 +0900

    ???

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
-import { Board } from '#/Board';
-
+import { Board } from "#/Board";
 
 const board = new Board(10);
-const items: NodeListOf<Element> = document.querySelectorAll('.item');
+const items: NodeListOf<Element> = document.querySelectorAll(".item");
 const allItems: Array<HTMLElement> = [].slice.call(items);
-const element: HTMLElement | null = document.querySelector('.container');
-const resultElement: HTMLElement | null = document.querySelector('.result');
+const element: HTMLElement | null = document.querySelector(".container");
+const resultElement: HTMLElement | null = document.querySelector(".result");
 
 if (!!element) {
-    element.addEventListener('click', (e: Event): void => {
-        const elem = e.target as HTMLElement;
-        const idx: number = allItems.indexOf(elem);
+    element.addEventListener(
+        "click",
+        (e: Event): void => {
+            const elem = e.target as HTMLElement;
+            const idx: number = allItems.indexOf(elem);
 
-        if (board.exists(idx)) {
-            return;
-        }
-        const block = `x`;
-        board.setData(idx, block);
-
-        elem.innerHTML = idx.toString();
-        elem.classList.add('block-first');
-
-        if (board.isGameEnd(idx) === true) {
-            if (!!resultElement) {
-                resultElement.innerHTML = block + ` の勝利!`;
+            if (board.exists(idx)) {
+                return;
             }
-            return;
-        }
-    }, false);
+            const block = `x`;
+            board.setData(idx, block);
+
+            elem.innerHTML = idx.toString();
+            elem.classList.add("block-first");
+
+            if (board.isGameEnd(idx) === true) {
+                if (!!resultElement) {
+                    resultElement.innerHTML = block + ` の勝利!`;
+                }
+                return;
+            }
+        },
+        false
+    );
 }
-
-
